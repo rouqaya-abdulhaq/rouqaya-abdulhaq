127e7645503dddeb31f7ddc5b07495655e1fe548
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-03-15 15:29:10 +0300

    set up basic logo component

---
+++ src/components/UI/logo/logo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const logo = (props) =>{
+    return(
+        <p>LOGO</p>
+    );
+}
+
+export default logo;
--- src/components/layout/header/header.js
+++ src/components/layout/header/header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import NavButton from '../../UI/navButton/navButton';
+import Logo from '../../UI/logo/logo';
 
 const Header = () => {
     return(
         <div>
-             <Link to="/">logo  </Link>
+             <Link to="/"><Logo/></Link>
              <div>
                <Link to="/about-me"><NavButton value={"ABOUT ME"}/></Link> 
                <Link to="/projects"><NavButton value={"PROJECTS"}/></Link> 
