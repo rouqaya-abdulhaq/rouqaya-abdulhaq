eb8d1b826f35211f91a6c9ebb9f7f3eedd04ce48
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-03-15 15:42:03 +0300

    basic header setup

--- src/components/UI/logo/logo.js
+++ src/components/UI/logo/logo.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const logo = (props) =>{
     return(
-        <p>LOGO</p>
+        <p className="logo">LOGO</p>
     );
 }
 
--- src/components/layout/header/header.js
+++ src/components/layout/header/header.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 import NavButton from '../../UI/navButton/navButton';
 import Logo from '../../UI/logo/logo';
 
+import './header.css';
+
 const Header = () => {
     return(
-        <div>
-             <Link to="/"><Logo/></Link>
-             <div>
-               <Link to="/about-me"><NavButton value={"ABOUT ME"}/></Link> 
-               <Link to="/projects"><NavButton value={"PROJECTS"}/></Link> 
+        <div className="header">
+            <div className="logoArea">
+                <Link to="/"><Logo/></Link> 
+            </div>
+            <div></div>
+            <div className="navArea">
+                <Link to="/about-me"><NavButton value={"ABOUT ME"}/></Link> 
+                <Link to="/projects"><NavButton value={"PROJECTS"}/></Link> 
             </div>  
         </div>
     );
