1d384bb18a82a89988f03bd080708772e5c69328
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-03-16 15:15:59 +0300

    basic styling footer setup

--- src/components/layout/footer/footer.js
+++ src/components/layout/footer/footer.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Link from '../../UI/link/link';
+import './footer.css';
 
 const footer = () => {
     return(
-        <div>
-            <div>license</div>
-            <div>
+        <footer>
+            <div className="license">license</div>
+            <div className="socialMediaLinks">
                 <Link to="Linked In"/>
                 <Link to="Twitter"/>
                 <Link to="GitHub"/>
             </div>
-        </div>
+        </footer>
     );
 }
 
