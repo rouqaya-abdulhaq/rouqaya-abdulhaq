43a68b450f1e80fd3a18da7102c6604ff2da0e1e
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-03-21 17:37:33 +0300

    basic textArea component

---
+++ src/components/UI/textArea/textArea.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './textArea.css';
+
+const textArea = (props) =>{
+    return(
+        <textarea>eneter text here</textarea>
+    );
+}
+
+export default textArea
