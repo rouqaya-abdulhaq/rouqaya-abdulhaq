3059fbf07e2978c3a04404a5e5a9cb7a5d9e6fa9
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-03-23 16:25:24 +0300

    redirected to blog page when a blog is added

--- src/containers/blogs/addBlog/addBlog.js
+++ src/containers/blogs/addBlog/addBlog.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import TextArea from '../../../components/UI/textArea/textArea';
+import SubmitBtn from '../../../components/UI/buttons/button/button';
+import { withRouter } from 'react-router-dom'
 
 class AddBlog extends React.Component{
     constructor(props){
@@ -30,14 +32,21 @@ class AddBlog extends React.Component{
         })
     }
 
+    submitBlogHandler = () =>{
+        console.log(this.state.blog.title);
+        console.log(this.state.blog.content);
+        this.props.history.replace('/blogs/blog');
+    }
+
     render(){
         return(
             <main>
                 <input onChange={this.titleEventHandler} placeholder="title"></input>
                 <TextArea onChange={this.contentEventHandler}/>
+                <SubmitBtn value="SUBMIT" onClick={this.submitBlogHandler}/>
             </main>
         );
     }
 }
 
-export default AddBlog;
+export default withRouter(AddBlog,);
