94a285a4fa920be2aea2779fd8c5e6292721346a
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-04-05 17:22:00 +0300

    simple animation for side bar

--- src/components/layout/sideBar/sideBar.js
+++ src/components/layout/sideBar/sideBar.js
@@ -2,27 +2,29 @@ import React from 'react';
 import './sideBar.css';
 import Modal from '../../HOC/modal/modal';
 
+
+
 const sideBar = (props) =>{
-    if(props.showSideBar){
+
+        let showSideBar = props.showSideBar ? {left : 0} : {};
+        
         return (
-            <div className="sideBar">
-               <Modal>
-                    <ul className="sideBar"> 
-                        <li className="unToggle" onClick={props.toggleSideBarHandler}> X </li>
-                        <li>About</li>
-                        <li>Projects</li>
-                        <li>Blog</li>
-                    </ul>
-                </Modal> 
+            <div className="slide">
+                <div className="toggle">
+                    <div onClick={props.toggleSideBarHandler} className="sideBar">|||</div>
+                </div>
+                <div id="menu" style={showSideBar}>
+                    <Modal className="modal">
+                        <ul className="sideBar"> 
+                            <li className="unToggle" onClick={props.toggleSideBarHandler}> X </li>
+                            <li>About</li>
+                            <li>Projects</li>
+                            <li>Blog</li>
+                        </ul>
+                    </Modal> 
+                </div> 
             </div>
         );
-    }else{
-        return(
-            <div className="toggle">
-                <div onClick={props.toggleSideBarHandler} className="sideBar">|||</div>
-            </div>
-        );
-    }
 }
 
 export default sideBar;
