9f147848a6abd62dae6159b70b82dcdfb4bb0e94
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-06-07 16:22:35 +0300

    loaded projects from the server in landing

--- src/components/layout/landing/landing.js
+++ src/components/layout/landing/landing.js
@@ -1,24 +1,52 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Card from '../../UI/card/card';
 import './landing.css';
 
-const landingPage = (props) => {
+const LandingPage = (props) => {
+
+    const [hasError, setError] = useState(false);
+    const [blogs, setBlogs] = useState([]);
+    
+    
+    useEffect(() => {
+        fetch(`http://localhost:8000/loadLastBlogs`,{
+        method : 'GET',
+        headers : {
+            'Accept': 'application/json',
+        }
+        }).then((res)=>{
+            return res.json();
+        }).then((blogs) => {
+            setBlogs(blogs);
+        }).catch((err)=>{
+            setError(err);
+            console.log(hasError);
+        });
+    });
+
+    let blogsToRender = [];
+    if(blogs){
+        blogsToRender = blogs.map((blog)=>{
+            return <Card key={blog.title} title={blog.title} />
+        })
+    } 
+
     return(
         <main>
             <h1>welcome to my online presence</h1>
             <section className="blogsArea">
                 <div className="blogsTape">
                     <div className="firstAnimation">
-                        <Card info="my first blog" />
+                        {blogsToRender[0] ? blogsToRender[0] : null}
                     </div>
                   <div className="secondAnimation">
-                        <Card info="my second blog" />
+                        {blogsToRender[1] ? blogsToRender[1] : null}
                     </div>
                     <div className="thirdAnimation">
-                        <Card info="my third blog" />
+                        {blogsToRender[2] ? blogsToRender[2] : null}
                     </div>
                     <div className="fourthAnimation">
-                        <Card info="my fourth blog" />
+                        {blogsToRender[3] ? blogsToRender[3] : null}
                     </div>
                 </div>
             </section>
@@ -26,4 +54,4 @@ const landingPage = (props) => {
     );
 }
 
-export default landingPage;
+export default LandingPage;
