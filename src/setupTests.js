638c00041584cb0522350dded363f3ea87e79f83
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-07-06 17:32:16 +0300

    setup testing env

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,7 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
