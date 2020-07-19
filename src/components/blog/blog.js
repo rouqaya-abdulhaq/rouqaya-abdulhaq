18a2b4a449a3fe73223011e661f54ecb1affd431
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-07-19 17:05:39 +0300

    added arabic translation to blogs

--- src/components/blog/blog.js
+++ src/components/blog/blog.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import BackBtn from '../UI/buttons/button/button';
+import Btn from '../UI/buttons/button/button';
 import {withRouter} from 'react-router-dom';
 import {FacebookShareButton,TwitterShareButton,LinkedinShareButton} from 'react-share';
 import queryString from 'query-string';
@@ -13,7 +13,8 @@ import './blog.css';
 const Blog = withRouter(({history,location, ...props}) =>{
 
     const [hasError, setError] = useState(false);
-    const [blog, setBlog] = useState({}); 
+    const [blog, setBlog] = useState({});
+    const [translate , setTranslate] = useState(false); 
 
     const values = queryString.parse(location.search);
 
@@ -21,26 +22,57 @@ const Blog = withRouter(({history,location, ...props}) =>{
          history.goBack();
     }
 
-    useEffect(() => {
-        let mounted = true;
-        fetch(`http://localhost:8000/loadBlog?blogId=${values.id}`,{
-        method : 'GET',
-        headers : {
-            'Accept': 'application/json',
-        }
-        }).then((res)=>{
-            return res.json();
-        }).then((res) => {
-            if(mounted){
-                if(res.success){
-                    setBlog(res.blog); 
-                }
+    const translateHandler = () =>{
+        setTranslate(!translate);
+    }
+
+    if(!translate){
+        useEffect(() => {
+            let mounted = true;
+            fetch(`http://localhost:8000/loadBlog?blogId=${values.id}`,{
+            method : 'GET',
+            headers : {
+                'Accept': 'application/json',
             }
-        }).catch((err)=>{
-            setError(err);
-        });
-        return () => mounted = false;
-    })
+            }).then((res)=>{
+                return res.json();
+            }).then((res) => {
+                if(mounted){
+                    if(res.success){
+                        setBlog(res.blog); 
+                    }
+                }
+            }).catch((err)=>{
+                setError(err);
+            });
+            return () => mounted = false;
+        }) 
+    }else if(translate){
+        useEffect(() => {
+            let mounted = true;
+            fetch(`http://localhost:8000/getArabicBlog?blogId=${values.id}`,{
+            method : 'GET',
+            headers : {
+                'Accept': 'application/json',
+            }
+            }).then((res)=>{
+                return res.json();
+            }).then((res) => {
+                if(mounted){
+                    if(res.success){
+                        setBlog(res.blog); 
+                    }else{
+                        setBlog({title : "لا يوجد ترجمة", content : "سوف نقوم بالترجمة بأسرع وقت"});
+                    }
+                }
+            }).catch((err)=>{
+                setError(err);
+            });
+            return () => mounted = false;
+        }) 
+    }
+
+    const translationBtn = translate ? <Btn onClick={translateHandler} value="English"/> : <Btn onClick={translateHandler} value="العربية"/>
 
     const content = hasError ? <ServerErr data={"blog"}/> 
                         : 
@@ -49,6 +81,7 @@ const Blog = withRouter(({history,location, ...props}) =>{
                                 <p>
                                     {blog.content}
                                 </p> 
+                                {translationBtn}
                                 <div className="share">
                                     
                                     <FacebookShareButton children={<img src={facebook} alt="share on facebook" title="share"/>} url={window.location.href} 
@@ -65,7 +98,7 @@ const Blog = withRouter(({history,location, ...props}) =>{
     
     return(
         <main className="blog">
-            <BackBtn onClick={backEventHandler} value="back"/>
+            <Btn onClick={backEventHandler} value="back"/>
             {content}
         </main>
     );
